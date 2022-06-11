168408611eb5baf367106e71b7889d1712600bd4
diogo-soares <diogosoaresk8@hotmail.com>
2022-06-10 23:17:54 -0300

    news func

---
+++ pkg/user/user.go
@@ -0,0 +1,23 @@
+package user
+
+var ErrorMethodNotAllowed = "method not allowed"
+
+func FetchUser() {
+
+}
+
+func FetchUsers() {
+
+}
+
+func CreateUser() {
+
+}
+
+func UpdateUser() {
+
+}
+
+func DeleteUser() error {
+
+}
